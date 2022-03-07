cf6784a06a163f44901aefc399950066343e4225
Your Name <you@example.com>
2022-03-07 13:40:25 +0400

    Nest

---
+++ Nest-Project/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".news").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        vertical: true,
+        arrows: false
+    });
+});
