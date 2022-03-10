9b1b7cd88c202e2b4a9d65d6899638181787473f
Amil Nuriyev <anuriyev008@gmail.com>
2022-03-10 23:25:40 +0400

    Nest v2

--- Nest-Project/js/main.js
+++ Nest-Project/js/main.js
@@ -8,3 +8,15 @@ $(function() {
         arrows: false
     });
 });
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+  });
+});
