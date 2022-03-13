a3c3e8ba15e774c5bd2f1d3963a5ed93ab902fec
Amil Nuriyev <anuriyev008@gmail.com>
2022-03-13 20:12:30 +0400

    Nest v3

--- Nest-Project/js/main.js
+++ Nest-Project/js/main.js
@@ -8,15 +8,39 @@ $(function() {
         arrows: false
     });
 });
-
 $(document).ready(function(){
   $('.carousel').slick({
     dots: false,
     infinite: true,
     autoplay: true,
-    speed: 700,
+    speed: 900,
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
   });
 });
+
+$(document).ready(function(){
+  $('.carousel-product').slick({
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    speed: 900,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+  });
+});
+
+let mapOp = {
+  center: [38.752, 48.850],
+  zoom: 10,
+}
+
+let map = new L.map('map' , mapOp);
+
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+let marker = new L.Marker([51.958, 9.141]);
+marker.addTo(map);
